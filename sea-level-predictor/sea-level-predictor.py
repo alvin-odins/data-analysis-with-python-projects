800dbe01e2f4115409bb77ea34f61227f28f3739
alvin-odins <alvin.odins@gmail.com>
2025-03-17 17:49:18 +0100

    performed data cleaning and created scatter plot

--- sea-level-predictor/sea-level-predictor.py
+++ sea-level-predictor/sea-level-predictor.py
@@ -4,9 +4,14 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
 
+    # data cleaning: update null NOAA values with mean of lower and upper
+    mean_error_bound = (df['Lower Error Bound'] + df['Upper Error Bound']) / 2
+    df['NOAA Adjusted Sea Level'] = df['NOAA Adjusted Sea Level'].fillna(mean_error_bound)
 
     # Create scatter plot
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
 
     # Create first line of best fit
